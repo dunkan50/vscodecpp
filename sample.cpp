f1b47ae24e2b55bdf78c8e77dae0bf02b829fe5e
dunkan50 <dunkim2008@gmail.com>
2017-06-24 12:47:46 +0300

    part solution

---
+++ sample.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age;
+    char name;
+    cout<<"what is your age";
+    cin>>age;
+    cout<<age<<endl;
+    cout<<"welcome here you old enough"<<age<<endl;
+    cout<<"what is your name: "<<endl;
+    cin>>name; 
+    cout<<"hello: "<<name<<endl;   
+    cout<<"hello world"<<name<<endl;
+    return 0;
+}
